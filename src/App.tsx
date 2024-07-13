148b65b09b3333ac0aae75897c2f510d5b630672
Francesco Valerio <francescovalerio.l@outlook.com>
2024-07-13 23:06:53 +0200

    test lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
+import { Suspense, lazy } from "react";
 import "./App.scss";
-import Navbar from "./components/Navbar";
+import Loading from "./components/Loading";
+const Navbar = lazy(() => import("./components/Navbar"));
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
-      <button type="button" className="btn btn-secondary">
-        Secondary
-      </button>
-      <button type="button" className="btn btn-success">
-        Success
-      </button>
-      <button type="button" className="btn btn-danger">
-        Danger
-      </button>
-      <button type="button" className="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" className="btn btn-info">
-        Info
-      </button>
-      <button type="button" className="btn btn-light">
-        Light
-      </button>
-      <button type="button" className="btn btn-dark">
-        Dark
-      </button>
+      <Suspense fallback={<Loading />}>
+        <Navbar />
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+        <button type="button" className="btn btn-secondary">
+          Secondary
+        </button>
+        <button type="button" className="btn btn-success">
+          Success
+        </button>
+        <button type="button" className="btn btn-danger">
+          Danger
+        </button>
+        <button type="button" className="btn btn-warning">
+          Warning
+        </button>
+        <button type="button" className="btn btn-info">
+          Info
+        </button>
+        <button type="button" className="btn btn-light">
+          Light
+        </button>
+        <button type="button" className="btn btn-dark">
+          Dark
+        </button>
+      </Suspense>
     </div>
   );
 }
